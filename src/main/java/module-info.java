c0e0b787751b955f83d9a131d5fd8747da8b24cf
serrrrrgio <sergio.posadag@uqvirtual.edu.co>
2025-10-17 07:31:58 -0500

    Se crea el Proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.parcial2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.parcial2 to javafx.fxml;
+    exports co.edu.uniquindio.poo.parcial2;
+}
